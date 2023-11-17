12681cb888a8fdb8d09f8694bc30684b1618802b
Ben Wang <firetruckbw@gmail.com>
2023-11-17 20:56:53 +0000

    Implement CORS for External Requests

--- SpringDataJPA/src/main/java/com/tutoring/springdatajpa/SpringDataJpaApplication.java
+++ SpringDataJPA/src/main/java/com/tutoring/springdatajpa/SpringDataJpaApplication.java
@@ -11,6 +11,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Date;
 
@@ -20,6 +22,17 @@ public class SpringDataJpaApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringDataJpaApplication.class, args);
     }
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*").allowedHeaders("*").allowedMethods("*");
+			}
+		};
+	}
+
 
     @Bean
     public CommandLineRunner run(UserRepository userRepository, TutorRepository tutorRepository, AppointmentRepository appointmentRepository) {
