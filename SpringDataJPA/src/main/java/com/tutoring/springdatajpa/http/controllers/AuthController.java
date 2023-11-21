74fa89cc9ed15a6b826ed4206d54a73e1ca82fdb
David-Brodsky <73510062+David-Brodsky@users.noreply.github.com>
2023-11-21 13:39:09 -0500

    One more try...

--- SpringDataJPA/src/main/java/com/tutoring/springdatajpa/http/controllers/AuthController.java
+++ SpringDataJPA/src/main/java/com/tutoring/springdatajpa/http/controllers/AuthController.java
@@ -14,6 +14,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
